6dd9e5ef1b4bf8fe94cc41802bf3efbc0a03bea4
Deeven Paul <pauldeeven7@gmail.com>
2019-04-08 11:32:47 -0400

    This is a react application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import Projectdetails  from './components/projects/Projectdetails';
+import Signin from './components/auth/Signin';
+import Signup from './components/auth/Signup';
+import Createmessage from './components/projects/Createmessage';
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar />
+        <Switch>
+         <Route exact path = '/' component = {Dashboard} />
+         <Route path = '/project/:id' component = {Projectdetails} />
+         <Route path = '/signin' component = {Signin} />
+         <Route path = '/signup' component = {Signup} />
+         <Route path = '/create' component = {Createmessage} />
+        </Switch>
       </div>
+      </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore, applyMiddleware, compose} from 'redux'
+import Rootreducer from './store/reducers/Rootreducer';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import Firebase from './config/Firebase';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ const store = createStore(Rootreducer, 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
+    reduxFirestore(Firebase),
+    reactReduxFirebase(Firebase, {useFirestoreForProfile: true, userProfile: 'users', attachAuthIsReady: true})
+    
+    )
+);
+
+store.firebaseAuthIsReady.then(()=> {
+  ReactDOM.render(<Provider store = {store}><App /></Provider>, document.getElementById('root'));
+
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.unregister();
+})
+
+
